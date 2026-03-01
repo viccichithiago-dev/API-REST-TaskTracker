cfd607d55fbc9d1288a07f4c12c649d0fa908c5c
Thiago Viccichi <viccichithiago@gmail.com>
2026-03-01 15:24:53 -0300

    Implementancion de configuracion basica y autenticacion basico a futuro mejorar para usar JWT

--- src/main/java/com/thiago/tasktracker/model/User.java
+++ src/main/java/com/thiago/tasktracker/model/User.java
@@ -23,4 +23,30 @@ public class User {
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
+    // setters y getters generados por Lombok
+    public Long getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
---
+++ src/main/java/com/thiago/tasktracker/service/AuthService.java
@@ -0,0 +1,5 @@
+package com.thiago.tasktracker.service;
+
+public class AuthService {
+
+}
